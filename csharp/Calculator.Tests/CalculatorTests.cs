5dab99111367aa9e8843ca36b9dcd3970fdf6154
dlacour-visiativ <57131603+dlacour-visiativ@users.noreply.github.com>
2019-11-19 16:30:54 +0100

    Add calculator and calculator.tests projects

---
+++ csharp/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+	[TestClass]
+	public class CalculatorTests
+	{
+		[TestMethod]
+		public void shouldAlwaysFail()
+		{
+			Assert.IsTrue(false);
+		}
+
+		[TestMethod]
+		public void addition_1and3_returns4()
+		{
+			Calculator calculator = new Calculator();
+
+			int result = calculator.addition(1, 3);
+
+			Assert.AreEqual(4, result);
+		}
+
+		[TestMethod]
+		public void addition_2and3_returns5()
+		{
+			Calculator calculator = new Calculator();
+
+			int result = calculator.addition(2, 3);
+
+			Assert.AreEqual(5, result);
+		}
+	}
+}
